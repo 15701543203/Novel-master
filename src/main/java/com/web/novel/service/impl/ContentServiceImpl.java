260adf39c20eaf5a3316b4de1817a6a1a60ee829
lizhen <lizhen_init@163.com>
2018-03-01 17:32:11 +0800

    修改

--- src/main/java/com/web/novel/service/impl/ContentServiceImpl.java
+++ src/main/java/com/web/novel/service/impl/ContentServiceImpl.java
@@ -14,55 +14,45 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 
 @Service
-public class ContentServiceImpl implements ContentService{
-    @Autowired
-   private ChapterMapper chapterMapper;
-    @Autowired
-    private BookMapper bookMapper;
+public class ContentServiceImpl implements ContentService {
+	@Autowired
+	private ChapterMapper chapterMapper;
+	@Autowired
+	private BookMapper bookMapper;
 
-    public String getContentByChapterId(int chapterId){
-        // 章节路径
+	public String getContentByChapterId(int chapterId) {
+		// 章节路径
+		Chapter chapter = chapterMapper.selectByPrimaryKey(chapterId);
+		String fileName = chapter.getChapterUrl();
+		StringBuffer sb = new StringBuffer();
+		int bookId = chapter.getBookId();
+		Book book = bookMapper.selectByPrimaryKey(bookId);
+		String path = book.getBookUrl();
+		//查询整本书的路劲，截取之后拼接成章节的路径
+		path = path.substring(0, path.lastIndexOf("/") + 1);
+		System.err.println(path + fileName);
 
-        Chapter chapter = chapterMapper.selectByPrimaryKey(chapterId);
-        String fileName = chapter.getChapterUrl();
-        StringBuffer sb = new StringBuffer();
-        int bookId = chapter.getBookId();
-        Book book = bookMapper.selectByPrimaryKey(bookId);
-        String path = book.getBookUrl();
-         path = path.substring(0,path.lastIndexOf("/")+1);
-        System.out.println(path+ fileName);
+		try {
+			// 从本地磁盘读取
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(new FileInputStream(path + fileName), "UTF-8"));
+			String line = null;
+			int i = 0;
+			sb.append("<center><h3>" + chapter.getChapterName() + "</h3></center>");
+			while ((line = br.readLine()) != null) {
+				if (!line.isEmpty()) {
+					line = "<p>" + line + "</p>";
+				}
+				System.out.println(line);
+				sb.append(line);
+				i++;
+			}
+			System.out.println(i);
+			br.close();
+		} catch (Exception e) {
+		}
+		return sb.toString();
 
-        try {
-            // 从本地磁盘读取
-
-
-
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    new FileInputStream(path+ fileName), "UTF-8"));
-            String line = null;
-            int i =0;
-            sb.append("<center><h3>"+chapter.getChapterName()+"</h3></center>");
-            while ((line = br.readLine()) != null) {
-                if (!line.isEmpty()){
-                line = "<p>" + line + "</p>";
-                }
-                System.out.println(line);
-                sb.append(line);
-                i++;
-            }
-            System.out.println(i);
-            br.close();
-        } catch (Exception e) {
-        }
-        return sb.toString();
-
-    }
-
-
-
-
-
-
+	}
 
 }
